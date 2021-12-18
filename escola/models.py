623c83a4583badc68f115b96a96b96fe4e655833
Lucas <lucas.beckhauser@projuris.com>
2021-12-18 18:46:12 -0300

    Criando modelo de Aluno e Curso

---
+++ escola/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Aluno(models.Model):
+  nome = models.CharField(max_length=30)
+  rg = models.CharField(max_length=30)
+  cpf = models.CharField(max_length=30)
+  data_nascimento = models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.name
+
+class Curso(models.Model):
+  NIVEL = (
+    ('B', 'Básico'),
+    ("I", "Intermediário"), 
+    ('A', 'Avançado'))
+  codigo_curso = models.CharField(max_length=10)
+  descricao = models.CharField(max_length=100)
+  nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
+  
+
+  
